99855234195619cbc752066a9fef8f510ecda7fc
OhnkytaBlabdey <1263189143@qq.com>
2019-02-03 18:11:52 +0800

    add external support

---
+++ Game/ExternalsUtil.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindBot.Game
+{
+    class ExternalsUtil
+    {
+        static public int Choose(int count)
+        {
+            int res = 0;
+
+            return res;
+        }
+    }
+}
