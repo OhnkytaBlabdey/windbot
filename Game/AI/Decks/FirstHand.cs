3457092b60074ed9372b54464be441544e6ec182
OhnkytaBlabdey <1263189143@qq.com>
2019-02-15 15:53:55 +0800

    add decks

---
+++ Game/AI/Decks/FirstHand.cs
@@ -0,0 +1,27 @@
+﻿using YGOSharp.OCGWrapper.Enums;
+using System.Collections.Generic;
+using System.Linq;
+using WindBot;
+using WindBot.Game;
+using WindBot.Game.AI;
+
+namespace WindBot.Game.AI.Decks
+{
+    [Deck("FirstHand", "first_hand")]
+    class FirstHand : Executor
+    {
+        FirstHand(GameAI ai, Duel duel)
+            : base(ai, duel)
+        {
+
+        }
+        public override int OnRockPaperScissors()
+        {
+            return 1;
+        }
+        public override bool OnSelectHand()
+        {
+            return true;
+        }
+    }
+}
