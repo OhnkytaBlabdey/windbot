ffc78573c63e0e0cae71b46566c9803d10367e2e
OhnkytaBlabdey <1263189143@qq.com>
2019-02-19 23:36:09 +0800

    master rule 3 handle

---
+++ Game/AI/Decks/SecondHandN.cs
@@ -0,0 +1,26 @@
+﻿using YGOSharp.OCGWrapper.Enums;
+using System.Collections.Generic;
+using System.Linq;
+using WindBot;
+using WindBot.Game;
+using WindBot.Game.AI;
+namespace WindBot.Game.AI.Decks
+{
+    [Deck("SecondHandN", "AI_SecondHandN")]
+    class SecondHandN : DefaultExecutor
+    {
+        public SecondHandN(GameAI ai, Duel duel)
+            : base(ai, duel)
+        {
+
+        }
+        public override bool OnSelectHand()
+        {
+            return false;
+        }
+        public override int OnRockPaperScissors()
+        {
+            return 2;
+        }
+    }
+}
